b8ad95f7bac5b8ae54d8156ce1dfa045c104f22c
cooljasonmelton <cooljasonmelton>
2020-03-24 12:09:49 -0500

    Added new and edit for moves

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -9,19 +9,41 @@ class MovesController < ApplicationController
     end 
 
     def new
+        @move = Move.new
     end 
 
     def create
+        @move = Move.new(move_params)
+        if @move.valid?
+            @move.save
+            redirect_to move_path(@move)
+        else
+            #errors
+            render :new 
+        end 
     end 
 
     def edit
+        find_move
     end
 
     def update
+        find_move.assign_attributes(move_params)      
+        if @move.valid?
+            @move.save
+            redirect_to move_path(@move)
+        else
+            #@errors
+            render :new 
+        end 
     end
 
     private
     def find_move
         @move = Move.find(params[:id])
+    end 
+
+    def move_params
+        params.require(:move).permit(:name, :description, :img_url)
     end 
 end
--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -9,9 +9,18 @@ class WrestlersController < ApplicationController
     end 
 
     def new
+        @wrestler = Wrestler.new 
     end 
 
     def create
+        @wrestler = Wrestler.new(wrestler_params)
+        if @wrestler.valid?
+            @wrestler.save
+            redirect_to wrestler_path(@wrestler)
+        else 
+            # @errors
+            render :new
+        end 
     end 
 
     def edit
@@ -23,6 +32,10 @@ class WrestlersController < ApplicationController
     private
     def find_wrestler
         @wrestler = Wrestler.find(params[:id])
+    end 
+
+    def wrestler_params
+        params.require(:wrestler).permit(:name, :bio, :img_url)
     end 
 
 end
